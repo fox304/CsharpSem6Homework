40c32958d46fd2a8264fa785cd1f27fd4c8096d2
Maks <nordmax69@gmail.com>
2022-09-09 02:18:09 +0700

    rows per columns

---
+++ homework_7/Program.cs
@@ -0,0 +1,71 @@
+﻿// Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, 
+//                             что это невозможно (в случае, если матрица не квадратная).
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+void Fill(int [,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(10);
+        }
+    }
+}
+
+
+void Print(int [,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void Change(int [,] arr)
+{
+    int temp = 0;
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = i; j < arr.GetLength(1); j++)
+        {
+            temp = arr[i,j];
+            arr[i,j] = arr[j,i];
+            arr[j,i] = temp;
+            
+        }
+        
+        
+    }
+}
+
+int m = 0,n = 0;
+while (true)
+{
+    Console.WriteLine("Введите размеры матрицы : ");
+    Console.WriteLine("Введите m ");
+    m = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите n ");
+    n = int.Parse(Console.ReadLine());
+    if (m==n) break;
+    Console.WriteLine("Поскольку матрица не квадратная  , то заменить строки на столбцы невозможно ");
+    Console.WriteLine("попробуйте ещё раз  ");
+
+}
+Console.WriteLine("Итак , у нас такая матрица  ");
+int [,] array = new int [m,n];
+Fill(array);
+Print(array);
+Console.WriteLine();
+Console.WriteLine("После упорядочивания элементов получилась вот такая   ");
+Change(array);
+Console.WriteLine();
+Print(array);
