782295845552733f8b32e93f1906ecd95642a0ad
Maks <nordmax69@gmail.com>
2022-09-09 20:33:12 +0700

    comments to homework_8 and fixed errors

--- homework_8/Program.cs
+++ homework_8/Program.cs
@@ -31,47 +31,32 @@ void Print(int [,] arr)
 
 int Change(int [,] arr)
 {
-    int sum = 0,indexmin = 0,min = 0;
+    int sum = 0,indexmin = 0;
+    int min = 10000;
 
     for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = i; j < arr.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            sum += arr[i,j];   
+            sum += arr[i,j];  
+            
         }
+        Console.WriteLine($" сумма элементов {i+1} строки равна {sum} ");
         if (sum < min) 
         {
-            
+            min = sum;
             indexmin = i;
         }
+   
         sum = 0;
-        
         
     }
     return indexmin;
 }
 
-
-int [,] array = new int [4,3];
+Console.WriteLine($" Пусть дана матрица 7 на 3 ,тогда ");
+int [,] array = new int [7,3];
 Fill(array);
 Print(array);
-Console.WriteLine($" Строка с наименьшей суммой элементов номер : {Change(array)+1}");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Console.WriteLine();
+Console.WriteLine($" \nстрока с наименьшей суммой элементов номер : {Change(array)+1}");
